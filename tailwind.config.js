e3d937f89b58d5e058d70f691a3825bb95e2c563
Pawel <pawmag.code@gmail.com>
2025-10-11 13:21:03 +0200

    Fixed New Note View

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,19 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [{
+      dark: {
+        ...require("daisyui/src/theming/themes")["dark"],
+        "base-100": "#0f172a",
+        "base-200": "#1e293b",
+        "base-300": "#334155",
+        primary: "#0ea5e9",
+        secondary: "#ec4899",
+        accent: "#818cf8",
+      },
+    }],
+  },
 }
 
